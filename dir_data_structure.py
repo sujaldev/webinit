b82bb3a7415183ba10de87e14d7d6fca9dd9625b
Sujal Singh <email.sujalsingh@gmail.com>
2020-12-14 14:02:57 +0530

    tree reader partially working

--- dir_data_structure.py
+++ dir_data_structure.py
@@ -2,25 +2,28 @@ import os
 
 
 class Directory:
-    def __init__(self, parent, name):
-        if type(parent) == Directory:
+    def __init__(self, parent, name, direct="y"):
+        self.name = name
+        self.direct = direct
+        self.child_dirs = {}
+        self.files = {}
+        if type(parent) == Directory and self.direct == "y":
+            self.parent = parent
+            self.parent.add_dir(self.name)
             self.parent = parent.path
         else:
             self.parent = parent
-        self.name = name
         self.path = f"{self.parent}/{self.name}"
-        self.child_dirs = {}
-        self.files = {}
 
     def add_dir(self, dir_name):
-        self.child_dirs[dir_name] = Directory(self, dir_name)
+        self.child_dirs[dir_name] = Directory(self, dir_name, "n")
 
     def add_dirs(self, dir_list):
         for directory in dir_list:
             self.child_dirs[directory] = Directory(self, directory)
 
     def add_file(self, f_name, f_extension):
-        self.files[f_name] = File(self, f_name, f_extension)
+        self.files[f_name] = File(self, f_name, f_extension, "n")
 
     def add_files(self, file_dict):
         for file in file_dict:
@@ -38,10 +41,14 @@ class Directory:
 
 
 class File:
-    def __init__(self, file_parent, file_name, file_extension):
-        self.file_parent = file_parent.path
+    def __init__(self, file_parent, file_name, file_extension, direct="y"):
+        self.file_parent = file_parent
         self.file_name = file_name
         self.file_extension = file_extension
+        self.direct = direct
+        if self.direct == "y":
+            self.file_parent.add_file(self.file_name, self.file_extension)
+        self.file_parent = file_parent.path
         self.path = f"{self.file_parent}/{self.file_name}.{self.file_extension}"
 
     def make_file(self):
--- tree_reader.py
+++ tree_reader.py
@@ -1,32 +1,90 @@
 from dir_data_structure import Directory, File
-import os
-
-
-# keep track of current directory context
-# keep track of directory level
-
-def dir_level(dir_command):
-    level = dir_command[1:].count("--")
-    return level
 
 
 def is_dir(directory):
-    if directory[-1] == "/":
-        return True
+    return True if directory[-1] == "/" else False
+
+
+def level(item):
+    item_level = item[1:].count("--")
+    return item_level
+
+
+def file_extension(item):
+    file_ext = item[1:].replace("--"*level(item), '')
+    file_ext = file_ext.split(".")[-1]
+    return file_ext
+
+
+def item_name(item, full_name="n"):
+    if item[0] == "|" and is_dir(item):
+        return item[:-1].replace(f"|{'--'*level(item)}", '')
+    elif item[0] == "|" and not is_dir(item):
+        if "." in item and full_name == "n":
+            file_name = item[1:].replace("--"*level(item), '')
+            file_name = '.'.join(file_name.split(".")[:-1])
+            return file_name
+        elif "." in item and full_name == "y":
+            return item[1:].replace("--"*level(item), '')
+        else:
+            return item[1:].replace("--"*level(item), '')
     else:
-        return False
+        return item[:-1]
 
 
-def dir_name(directory):
-    directory[1:-1].replace("--"*dir_level(directory), '')
+def tree_to_dir_structure(grandparent, parent_name, profile="basic"):
+    # create parent
+    parent = Directory(grandparent, parent_name)
 
-
-def read_tree(g_parent_path, name, profile):
-    parent = Directory(g_parent_path, name)
-    current_dir = parent.path
+    # get tree data
     with open(f"../profiles/{profile}/tree.txt", "r") as tree_data:
-        tree = tree_data.read().split("\n")
-    for entry in tree:
-        current_level = dir_level(entry)
-        if is_dir(entry):
-            parent.add_dir(dir_name(entry))
+        tree_list = tree_data.read().split("\n")
+
+    # initialize variables
+    dir_objects = {'parent': parent}
+
+    # read loop
+    for i in range(len(tree_list)):
+        if i == 0:
+            continue
+        current_level = level(tree_list[i])
+        j = i
+        while j >= 0:
+            if level(tree_list[j])+1 == current_level:
+                current_parent = item_name(tree_list[j])
+                child = tree_list[i]
+                child_name = item_name(child)
+                if current_parent == "parent" and is_dir(child):
+                    new_dir = Directory(parent, child_name)
+                    dir_objects[child_name] = new_dir
+                elif current_parent == "parent" and not is_dir(child):
+                    File(parent, child_name, file_extension(child))
+                # if is_dir(child):
+                #     new_dir = Directory(dir_objects[current_parent], child_name)
+                #     dir_objects[child_name] = new_dir
+                # elif not is_dir(child):
+                #     File(dir_objects[current_parent], child_name, file_extension(child))
+                break
+            j -= 1
+    return parent
+
+
+# tree_to_dir_structure("E:/NIVZER/Projects/web_init", "parent")
+print(tree_to_dir_structure("E:/NIVZER/Projects/web_init", "parent").child_dirs)
+
+
+"""
+
+TREE                INDEX    DIR LEVEL    PARENT DIR
+parent/               0         0         N/A
+|--js/                1         1         parent
+|----packages/        2         2         js
+|------node/          3         3         packages
+|----script.js        4         2         js
+|--css/               5         1         parent
+|----style.css        6         2         css
+|--img/               7         1         parent
+|--index.html         8         1         parent
+
+ # To find parent get dir level (n) of current dir or file and then find the last dir with level n-1.
+"""
