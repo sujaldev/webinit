cc084779c544e80c89961ee98f1bb84612987415
Sujal Singh <email.sujalsingh@gmail.com>
2020-12-15 18:18:12 +0530

    added cli arguments

--- webinit.py
+++ webinit.py
@@ -1,8 +1,18 @@
+import argparse
 import tree_reader as tree
 
 
-def let_there_be_light(path: str, name: str):
-    universe = tree.make_tree(path, name)
+def let_there_be_light(path: str, name: str, profile="basic"):
+    universe = tree.make_tree(path, name, profile)
     universe.create_all()
     tree.enforce_template(universe)
     tree.enforce_variables(universe)
+
+
+parser = argparse.ArgumentParser(description="Make a project directory.")
+parser.add_argument('path', type=str, help='Specify path for project directory')
+parser.add_argument('name', type=str, help='Specify name for project directory')
+parser.add_argument('profile', type=str, help='Specify name for project directory')
+args = parser.parse_args()
+
+let_there_be_light(args.path, args.name, args.profile)
